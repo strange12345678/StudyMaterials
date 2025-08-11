1b72ac4f9e993b824e0e8b352baa510ae56006d3
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-08-11 09:09:51 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,10 +4,10 @@ from typing import List
 API_ID = int(os.getenv("API_ID", "22582906"))
 API_HASH = os.getenv("API_HASH", "e3096dde3e27c72a50e0e53d8ab23d6a")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://u94531550:dFucn85bhhBCn3nj@cluster0.h18ubn0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-1002739849822"))
 ADMIN_ID = int(os.getenv("ADMIN_ID", "7861690278"))
-PICS = (os.environ.get("PICS", "")).split()
+PICS = (os.environ.get("PICS", "https://i.ibb.co/ZRd3qRjS/photo-2025-08-02-18-04-42-7537167412588707856.jpg")).split()
 LOG_CHNL = int(os.getenv("LOG_CHNL", "-1002789550852"))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME", "darkworld008") # Without @
 IS_FSUB = bool(os.environ.get("FSUB", False))

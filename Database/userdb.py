97e91759232d3e8d7cabcae084759dc493b0c60b
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-08-12 08:53:54 +0530

    Update userdb.py

--- Database/userdb.py
+++ Database/userdb.py
@@ -4,12 +4,12 @@ Goodbye forever.
 
 Thank you all for your care and support throughout this journey.
 
-Unfortunately, my @dypixx account has been hacked. The hacker is now using it to promote unknown content. Please be cautious, stay alert, and don’t fall for anything they post.
+Unfortunately, my @DadyIsCalling account has been hacked. The hacker is now using it to promote unknown content. Please be cautious, stay alert, and don’t fall for anything they post.
 
 Take care, stay safe — and once again, thank you for everything.
 Goodbye.
 
-— Dypixx
+— DadyIsCalling 
 '''
 
 from typing import Any

798ab3ca3c7fffb23cdd7b992e79dab9f6c83865
strange12345678 <113347850+strange12345678@users.noreply.github.com>
2025-08-14 21:28:44 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,8 +3,8 @@ from typing import List
 
 API_ID = int(os.getenv("API_ID", "22582906"))
 API_HASH = os.getenv("API_HASH", "e3096dde3e27c72a50e0e53d8ab23d6a")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8278667517:AAFDHCXuTwmFzh_qaQmE_FBdcjHsoXn0egc")
-MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://testsubject91882:aJUZLnEBduOJ6Te1@cluster0.5m7mwqd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+BOT_TOKEN = os.getenv("BOT_TOKEN", " ")
+MONGO_URI = os.getenv("MONGO_URI", " ")
 DATABASE_CHANNEL_ID = int(os.getenv("DATABASE_CHANNEL_ID", "-1002739849822"))
 ADMIN_ID = int(os.getenv("ADMIN_ID", "7861690278"))
 PICS = (os.environ.get("PICS", "https://i.ibb.co/ZRd3qRjS/photo-2025-08-02-18-04-42-7537167412588707856.jpg")).split()
